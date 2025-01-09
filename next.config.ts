13d6a5cb565cf3c4cc546909698de7f99a349c49
Pedro Henrique Ferraz Machado <pedrotech@PedrosMacStudio.lan>
2025-01-08 16:55:19 -0800

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    GITHUB_ID: "Ov23liHVksBzgnKRlBMD",
+    GITHUB_SECRET: "92abd461a97858a27a3103ce742acd7dc442ffb4",
+  },
+
+  images: {
+    domains: ["avatars.githubusercontent.com"],
+  },
 };
 
 export default nextConfig;
